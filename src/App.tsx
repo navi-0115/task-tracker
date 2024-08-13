1a46c00d995d5994a0336650dbc8aac343166f6a
Raden-nvii <navinugraha15@gmail.com>
2024-08-13 14:57:14 +0700

    feat: add dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import { ModeToggle } from "./components/ModeToggle";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex flex-col">
+        <div className="flex-grow max-w-7xl w-full p-4 mx-auto">
+          <ModeToggle />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
